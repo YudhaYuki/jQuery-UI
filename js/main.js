bf802b363a66bd547e8baa5302a8f866809c0720
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-27 15:46:20 +0200

    Draggable: CONTAINMENT, GRID, HANDLE, OPACITY

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
     $('.draggable').draggable({
         // axis: "x"
-        cancel: ".not_this"
+        cancel: ".not_this",
+        containment: "parent",
+        grid: [50, 20],
+        handle: ".handle",
+        opacity: ".35"
     });
 
 });
