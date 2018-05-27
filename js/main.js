708e0317a8dfe89ff7c9fd250da0015db0b4fb43
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-27 15:56:49 +0200

    Draggable: Function position

--- js/main.js
+++ js/main.js
@@ -6,7 +6,18 @@ $(document).ready(function() {
         containment: "parent",
         grid: [50, 20],
         handle: ".handle",
-        opacity: ".35"
+        opacity: ".35",
+        drag: function(event, ui) {
+            console.log(ui.position);
+        }
     });
+
+
+    // $('.draggable').draggable("disable");
+
+    // $('.draggable').draggable("destroy");
+
+    // $('.draggable').draggable("enable");
+
 
 });
